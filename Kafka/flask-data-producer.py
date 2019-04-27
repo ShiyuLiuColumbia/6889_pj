936b84cff4a4e90646bb8c03f18547a76b4b1828
ShiyuLiu <shiyuliucu@gmail.com>
2019-04-27 19:38:00 -0400

    first push

---
+++ Kafka/flask-data-producer.py
@@ -0,0 +1,146 @@
+import atexit
+import logging
+import json
+import time
+import random
+import datetime
+
+from googlefinance import getQuotes
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from flask import (
+    Flask,
+    request,
+    jsonify
+)
+from flask.ext.cors import CORS, cross_origin
+
+from kafka import KafkaProducer
+from kafka.errors import (
+    KafkaError,
+    KafkaTimeoutError
+)
+
+logger_format = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=logger_format)
+logger = logging.getLogger('data-producer')
+logger.setLevel(logging.INFO)
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+# CORS(app)
+app.config.from_envvar('ENV_CONFIG_FILE')
+kafka_broker = app.config['CONFIG_KAFKA_ENDPOINT']
+topic_name = app.config['CONFIG_KAFKA_TOPIC']
+
+producer = KafkaProducer(
+    bootstrap_servers=kafka_broker
+)
+
+schedule = BackgroundScheduler()
+schedule.add_executor('threadpool')
+schedule.start()
+
+symbols = set()
+
+
+def shutdown_hook():
+    """
+    a shutdown hook to be called before the shutdown
+    """
+    try:
+        logger.info('Flushing pending messages to kafka, timeout is set to 10s')
+        producer.flush(10)
+        logger.info('Finish flushing pending messages to kafka')
+    except KafkaError as kafka_error:
+        logger.warn('Failed to flush pending messages to kafka, caused by: %s', kafka_error.message)
+    finally:
+        try:
+            logger.info('Closing kafka connection')
+            producer.close(10)
+        except Exception as e:
+            logger.warn('Failed to close kafka connection, caused by: %s', e.message)
+    try:
+        logger.info('shutdown scheduler')
+        schedule.shutdown()
+    except Exception as e:
+        logger.warn('Failed to shutdown scheduler, caused by: %s', e.message)
+
+
+# def fetch_price(symbol):
+#     """
+#     helper function to retrieve stock data and send it to kafka
+#     :param symbol: symbol of the stock
+#     :return: None
+#     """
+#     logger.debug('Start to fetch stock price for %s', symbol)
+#     try:
+#         price = json.dumps(getQuotes('AAPL'))
+#         logger.debug('Retrieved stock info %s', price)
+#         producer.send(topic=topic_name, value=price, timestamp_ms=time.time())
+#         logger.info('Sent stock price for %s to Kafka', symbol)
+#     except KafkaTimeoutError as timeout_error:
+#         logger.warn('Failed to send stock price for %s to kafka, caused by: %s', (symbol, timeout_error.message))
+#     except Exception:
+#         logger.warn('Failed to fetch stock price for %s', symbol)
+
+def fetch_price( symbol):
+    """
+    helper function to retrieve stock data and send it to kafka
+    :param producer: instance of a kafka producer
+    :param symbol: symbol of the stock
+    :return: None
+    """
+    logger.debug('Start to fetch stock price for %s', symbol)
+    try:
+        # price = json.dumps(getQuotes(symbol))
+
+        price = random.randint(30, 120)
+        timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%MZ')
+        payload = ('[{"StockSymbol":"AAPL","LastTradePrice":%d,"LastTradeDateTime":"%s"}]' % (price, timestamp)).encode('utf-8')
+
+        logger.debug('Retrieved stock info %s', price)
+        producer.send(topic=topic_name, value=payload, timestamp_ms=time.time())
+        logger.debug('Sent stock price for %s to Kafka', symbol)
+    except KafkaTimeoutError as timeout_error:
+        logger.warn('Failed to send stock price for %s to kafka, caused by: %s', (symbol, timeout_error.message))
+    except Exception:
+        logger.warn('Failed to fetch stock price for %s', symbol)
+
+
+@app.route('/<symbol>', methods=['POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def add_stock(symbol):
+    if not symbol:
+        return jsonify({
+            'error': 'Stock symbol cannot be empty'
+        }), 400
+    if symbol in symbols:
+        pass
+    else:
+        # symbol = symbol.encode('utf-8')
+        symbols.add(symbol)
+        logger.info('Add stock retrieve job %s, %s' % (symbol, type(symbol)))
+
+        schedule.add_job(fetch_price, 'interval', [symbol], seconds=1, id=symbol)
+    return jsonify(results=list(symbols)), 200
+
+
+@app.route('/<symbol>', methods=['DELETE'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def del_stock(symbol):
+    if not symbol:
+        return jsonify({
+            'error': 'Stock symbol cannot be empty'
+        }), 400
+    if symbol not in symbols:
+        pass
+    else:
+        symbols.remove(symbol)
+        schedule.remove_job(symbol)
+    return jsonify(results=list(symbols)), 200
+
+if __name__ == '__main__':
+    atexit.register(shutdown_hook)
+    app.run(host='0.0.0.0', port=app.config['CONFIG_APPLICATION_PORT'])
---
+++ Node.js/redis-kafka/index.js
@@ -0,0 +1,69 @@
+//receive data directly from kafka without redis
+
+var argv = require('minimist')(process.argv.slice(2));
+var port = argv['port'];
+var kafka_topic = argv['kafka_topic'];
+var kafka_broker = argv['kafka_broker'];
+
+
+// - setup dependency instances
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+
+// - setup kafka consumer
+var kafka = require('kafka-node');
+var Consumer = kafka.Consumer;
+var Client = kafka.Client;
+var client = new kafka.KafkaClient()
+console.log('Creating a kafka consumer');
+var consumer = new Consumer(
+		client,
+		[{topic:kafka_topic}]
+	);
+console.log('Subscribing to kafka topic %s', kafka_topic);
+consumer.on('message',function(message){
+    console.log('Received new data from kafka %s',JSON.stringify(message));
+    io.sockets.emit('data', JSON.stringify(message));	
+})
+
+
+
+// - setup webapp routing
+app.use(express.static(__dirname + '/public/'));
+console.log(__dirname + '/node_modules/jquery/dist/');
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/d3', express.static(__dirname + '/node_modules/d3'));
+app.use('/nvd3', express.static(__dirname + '/node_modules/nvd3/build/'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+// app.use((req, res, next) => {
+//     console.log(req);
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//     if (req.method === "OPTIONS") {
+//       res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//       return res.status(200).json({});
+//     }
+//     next();
+//   });
+  
+server.listen(port, function () {
+    console.log('Server started at port %d.', port);
+});
+
+// - setup shutdown hooks
+var shutdown_hook = function () {
+    console.log('Quitting Kafka consumer');
+    consumer.close();
+    console.log('Shutting down app');
+    process.exit();
+};
+
+process.on('SIGTERM', shutdown_hook);
+process.on('SIGINT', shutdown_hook);
+process.on('exit', shutdown_hook);
